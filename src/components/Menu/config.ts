1b161cfa3818e1ad4ecb3f6e6cd8db4dc5434182
Literally Collective <bluedragon.cstar@outlook.com>
2021-05-18 14:05:09 -0300

    Feat: initial commit

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,81 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: 'https://www.marmelade.finance/'
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    initialOpenState: true,
+    items: [
+      {
+        label: 'Exchange',
+        href: '/swap'
+      },
+      {
+        label: 'Liquidity',
+        href: '/pool'
+      }
+    ]
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: 'https://www.marmelade.finance/farms'
+  },
+  {
+    label: 'Nests',
+    icon: 'PoolIcon',
+    href: 'https://www.marmelade.finance/nests'
+  },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: 'https://www.marmelade.finance/lottery'
+  // },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'PancakeSwap',
+        href: 'https://pancakeswap.info/token/0x1fb2151f7cb40805905f9b731c373c5cc682aa57',
+      },
+      {
+        label: 'CoinGecko',
+        href: 'https://www.coingecko.com/en/coins/goose-finance',
+      },
+      {
+        label: 'CoinMarketCap',
+        href: 'https://coinmarketcap.com/currencies/goose-finance/',
+      },
+      {
+        label: 'AstroTools',
+        href: 'https://app.astrotools.io/pancake-pair-explorer/0x19e7cbecdd23a16dfa5573df54d98f7caae03019',
+      },
+    ]
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: "Github",
+        href: "https://github.com/marmeladeswap/",
+      },
+      {
+        label: "Docs",
+        href: "https://marmeladeswap.gitbook.io/goose-finance/",
+      },
+      {
+        label: "Blog",
+        href: "https://goosefinance.medium.com/",
+      },
+    ],
+  }  
+]
+
+export default config
---
+++ src/constants/eggPriceContracts.ts
@@ -0,0 +1,7 @@
+const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
+  cakeAddress: '0x1fB2151f7cB40805905f9b731c373C5Cc682AA57',
+  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+  lpAddress: '0x4736BdC59aaD48228fAd288d25C1E7b0636ab248'
+}
+
+export default priceContracts
